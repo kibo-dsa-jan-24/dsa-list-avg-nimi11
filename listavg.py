d76cde0d326aef2d8079ab6dccff7a4d562585a2
Toluwanimi Sonuga <sonugatoluwanimi@gmail.com>
2024-01-09 12:11:55 +0100

    fn

--- listavg.py
+++ listavg.py
@@ -1,16 +1,16 @@
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
+        self.total = sum(lst)  # initialize total with the sum of the initial list
+        self.length = len(lst)  # initialize length with the length of the initial list
 
     def add(self, num):
         self.lst.append(num)
+        self.total += num  # update total with the added number
+        self.length += 1  # update length
 
     def compute_avg(self):
-        total = 0
-        for num in self.lst:
-            total += num
-        return total / len(self.lst)
+        return self.total / self.length
 
     def compute_avg_faster(self):
-        # implement this method
-        pass
+        return self.total / self.length
